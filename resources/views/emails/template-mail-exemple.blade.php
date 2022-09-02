95e10e668f3fdff71f45211524a220cc72a80c68
NicoP59 <nicolaspayne06@gmail.com>
2022-09-02 11:53:07 +0100

    page oublie mdp et reset responsive ok

--- resources/views/emails/template-mail-exemple.blade.php
+++ resources/views/emails/template-mail-exemple.blade.php
@@ -1,4 +1,8 @@
 
+
+<div class="col one">
+      <img src="img/vault-tec.png" alt="76 Systems" width="740" height="729" id="logo-v"  />
+</div>
 <h1> message : {{ $data['message'] }}</h1>
 <h2> subject : {{ $data['subject'] }}</h2>
 <h2> email : {{ $data['email'] }}</h2>
